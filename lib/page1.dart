579fad0b90248e7789a1628aa4f5a50714c358bb
ryo072144 <84168405+ryo072144@users.noreply.github.com>
2022-07-21 10:25:13 +0900

    Update page1.dart

--- lib/page1.dart
+++ lib/page1.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_exercise_2/widget.dart';
+import 'package:flutter_exercise_2/constants.dart';
 
 class Page1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(centerTitle: true, title: Text('page1', style: titleStyle(),),),
+      appBar: AppBar(centerTitle: true, title: Text('page1', style: titleStyle,),),
       body: Container(),
     );
   }
