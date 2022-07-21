59d70ca7f66aa76ae3ed830eae373aedf9d33fc0
ryo072144 <84168405+ryo072144@users.noreply.github.com>
2022-07-21 10:24:01 +0900

    Update and rename widget.dart to constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+//constは値の変更が不可能な変数。
+const TextStyle titleStyle = TextStyle(color: Color(0xff222222), fontSize: 20 ,fontWeight: FontWeight.bold);
--- lib/widget.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
-TextStyle titleStyle(){
-  return TextStyle(color: Color(0xff222222), fontSize: 20 ,fontWeight: FontWeight.bold);
-}
